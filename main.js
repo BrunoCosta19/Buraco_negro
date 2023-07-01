3b6eb6faf537061dd571f5327c1c4602902b1e13
Bruno Costa <102918628+BrunoCosta19@users.noreply.github.com>
2023-07-01 17:43:31 -0300

    Buraco negro

---
+++ main.js
@@ -0,0 +1,437 @@
+"use strict"
+/*global THREE, SHADER_LOADER, Mustache, Stats, Detector, $, dat:false */
+/*global document, window, setTimeout, requestAnimationFrame:false */
+/*global ProceduralTextures:false */
+
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+function Observer() {
+    this.position = new THREE.Vector3(10,0,0);
+    this.velocity = new THREE.Vector3(0,1,0);
+    this.orientation = new THREE.Matrix3();
+    this.time = 0.0;
+}
+
+Observer.prototype.orbitalFrame = function() {
+
+    //var orbital_y = observer.velocity.clone().normalize();
+    var orbital_y = (new THREE.Vector3())
+        .subVectors(observer.velocity.clone().normalize().multiplyScalar(4.0),
+            observer.position).normalize();
+
+    var orbital_z = (new THREE.Vector3())
+        .crossVectors(observer.position, orbital_y).normalize();
+    var orbital_x = (new THREE.Vector3()).crossVectors(orbital_y, orbital_z);
+
+
+    return (new THREE.Matrix4()).makeBasis(
+        orbital_x,
+        orbital_y,
+        orbital_z
+    ).linearPart();
+};
+
+Observer.prototype.move = function(dt) {
+
+    dt *= shader.parameters.time_scale;
+
+    var r;
+    var v = 0;
+
+    // motion on a pre-defined cirular orbit
+    if (shader.parameters.observer.motion) {
+
+        r = shader.parameters.observer.distance;
+        v =  1.0 / Math.sqrt(2.0*(r-1.0));
+        var ang_vel = v / r;
+        var angle = this.time * ang_vel;
+
+        var s = Math.sin(angle), c = Math.cos(angle);
+
+        this.position.set(c*r, s*r, 0);
+        this.velocity.set(-s*v, c*v, 0);
+
+        var alpha = degToRad(shader.parameters.observer.orbital_inclination);
+        var orbit_coords = (new THREE.Matrix4()).makeRotationY(alpha);
+
+        this.position.applyMatrix4(orbit_coords);
+        this.velocity.applyMatrix4(orbit_coords);
+    }
+    else {
+        r = this.position.length();
+    }
+
+    if (shader.parameters.gravitational_time_dilation) {
+        dt = Math.sqrt((dt*dt * (1.0 - v*v)) / (1-1.0/r));
+    }
+
+    this.time += dt;
+};
+
+var container, stats;
+var camera, scene, renderer, cameraControls, shader = null;
+var observer = new Observer();
+
+function Shader(mustacheTemplate) {
+    // Compile-time shader parameters
+    this.parameters = {
+        n_steps: 100,
+        quality: 'medium',
+        accretion_disk: true,
+        planet: {
+            enabled: true,
+            distance: 7.0,
+            radius: 0.4
+        },
+        lorentz_contraction: true,
+        gravitational_time_dilation: true,
+        aberration: true,
+        beaming: true,
+        doppler_shift: true,
+        light_travel_time: true,
+        time_scale: 1.0,
+        observer: {
+            motion: true,
+            distance: 11.0,
+            orbital_inclination: -10
+        },
+
+        planetEnabled: function() {
+            return this.planet.enabled && this.quality !== 'rápido';
+        },
+
+        observerMotion: function() {
+            return this.observer.motion;
+        }
+    };
+    var that = this;
+    this.needsUpdate = false;
+
+    this.hasMovingParts = function() {
+        return this.parameters.planet.enabled || this.parameters.observer.motion;
+    };
+
+    this.compile = function() {
+        return Mustache.render(mustacheTemplate, that.parameters);
+    };
+}
+
+function degToRad(a) { return Math.PI * a / 180.0; }
+
+(function(){
+    var textures = {};
+
+    function whenLoaded() {
+        init(textures);
+        $('#loader').hide();
+        $('.initially-hidden').removeClass('initially-hidden');
+        animate();
+    }
+
+    function checkLoaded() {
+        if (shader === null) return;
+        for (var key in textures) if (textures[key] === null) return;
+        whenLoaded();
+    }
+
+    SHADER_LOADER.load(function(shaders) {
+        shader = new Shader(shaders.raytracer.fragment);
+        checkLoaded();
+    });
+
+    var texLoader = new THREE.TextureLoader();
+    function loadTexture(symbol, filename, interpolation) {
+        textures[symbol] = null;
+        texLoader.load(filename, function(tex) {
+            tex.magFilter = interpolation;
+            tex.minFilter = interpolation;
+            textures[symbol] = tex;
+            checkLoaded();
+        });
+    }
+
+    loadTexture('galaxy', 'img/milkyway.jpg', THREE.NearestFilter);
+    loadTexture('spectra', 'img/spectra.png', THREE.LinearFilter);
+    loadTexture('moon', 'img/beach-ball.png', THREE.LinearFilter);
+    loadTexture('stars', 'img/stars.png', THREE.LinearFilter);
+    loadTexture('accretion_disk', 'img/accretion-disk.png', THREE.LinearFilter);
+})();
+
+var updateUniforms;
+
+function init(textures) {
+
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    scene = new THREE.Scene();
+
+    var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
+
+    var uniforms = {
+        time: { type: "f", value: 0 },
+        resolution: { type: "v2", value: new THREE.Vector2() },
+        cam_pos: { type: "v3", value: new THREE.Vector3() },
+        cam_x: { type: "v3", value: new THREE.Vector3() },
+        cam_y: { type: "v3", value: new THREE.Vector3() },
+        cam_z: { type: "v3", value: new THREE.Vector3() },
+        cam_vel: { type: "v3", value: new THREE.Vector3() },
+
+        planet_distance: { type: "f" },
+        planet_radius: { type: "f" },
+
+        star_texture: { type: "t", value: textures.stars },
+        accretion_disk_texture: { type: "t",  value: textures.accretion_disk },
+        galaxy_texture: { type: "t", value: textures.galaxy },
+        planet_texture: { type: "t", value: textures.moon },
+        spectrum_texture: { type: "t", value: textures.spectra }
+    };
+
+    updateUniforms = function() {
+        uniforms.planet_distance.value = shader.parameters.planet.distance;
+        uniforms.planet_radius.value = shader.parameters.planet.radius;
+
+        uniforms.resolution.value.x = renderer.domElement.width;
+        uniforms.resolution.value.y = renderer.domElement.height;
+
+        uniforms.time.value = observer.time;
+        uniforms.cam_pos.value = observer.position;
+
+        var e = observer.orientation.elements;
+
+        uniforms.cam_x.value.set(e[0], e[1], e[2]);
+        uniforms.cam_y.value.set(e[3], e[4], e[5]);
+        uniforms.cam_z.value.set(e[6], e[7], e[8]);
+
+        function setVec(target, value) {
+            uniforms[target].value.set(value.x, value.y, value.z);
+        }
+
+        setVec('cam_pos', observer.position);
+        setVec('cam_vel', observer.velocity);
+    };
+
+    var material = new THREE.ShaderMaterial( {
+        uniforms: uniforms,
+        vertexShader: $('#vertex-shader').text(),
+    });
+
+    scene.updateShader = function() {
+        material.fragmentShader = shader.compile();
+        material.needsUpdate = true;
+        shader.needsUpdate = true;
+    };
+
+    scene.updateShader();
+
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    container.appendChild( renderer.domElement );
+
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    container.appendChild( stats.domElement );
+    $(stats.domElement).addClass('hidden-phone');
+
+    // Orbit camera from three.js
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 80000 );
+    initializeCamera(camera);
+
+    cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+    cameraControls.target.set( 0, 0, 0 );
+    cameraControls.addEventListener( 'change', updateCamera );
+    updateCamera();
+
+    onWindowResize();
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    setupGUI();
+}
+
+function setupGUI() {
+
+    var hint = $('#hint-text');
+    var p = shader.parameters;
+
+    function updateShader() {
+        hint.hide();
+        scene.updateShader();
+    }
+
+    var gui = new dat.GUI();
+
+    gui.add(p, 'quality', ['fast', 'medium', 'high']).onChange(function (value) {
+        $('.planet-controls').show();
+        switch(value) {
+        case 'fast':
+            p.n_steps = 40;
+            $('.planet-controls').hide();
+            break;
+        case 'medium':
+            p.n_steps = 100;
+            break;
+        case 'high':
+            p.n_steps = 200;
+            break;
+        }
+
+        updateShader();
+    });
+    gui.add(p, 'accretion_disk').onChange(updateShader);
+
+    var folder = gui.addFolder('Observer');
+    folder.add(p.observer, 'motion').onChange(function(motion) {
+        updateCamera();
+        updateShader();
+        if (motion) {
+            hint.text('Moving observer; drag to rotate camera');
+        } else {
+            hint.text('Stationary observer; drag to orbit around');
+        }
+        hint.fadeIn();
+    });
+    folder.add(p.observer, 'distance').min(1.5).max(30).onChange(updateCamera);
+    folder.open();
+
+    folder = gui.addFolder('Planet');
+    folder.add(p.planet, 'enabled').onChange(function(enabled) {
+        updateShader();
+        var controls = $('.indirect-planet-controls').show();
+        if (enabled) controls.show();
+        else controls.hide();
+    });
+    folder.add(p.planet, 'distance').min(1.5).onChange(updateUniforms);
+    folder.add(p.planet, 'radius').min(0.01).max(2.0).onChange(updateUniforms);
+    $(folder.domElement).addClass('planet-controls');
+    //folder.open();
+
+    function setGuiRowClass(guiEl, klass) {
+        $(guiEl.domElement).parent().parent().addClass(klass);
+    }
+
+    folder = gui.addFolder('Relativistic effects');
+    folder.add(p, 'aberration').onChange(updateShader);
+    folder.add(p, 'beaming').onChange(updateShader);
+    folder.add(p, 'doppler_shift').onChange(updateShader);
+    setGuiRowClass(
+        folder.add(p, 'gravitational_time_dilation').onChange(updateShader),
+        'planet-controls indirect-planet-controls');
+    setGuiRowClass(
+        folder.add(p, 'lorentz_contraction').onChange(updateShader),
+        'planet-controls indirect-planet-controls');
+
+    folder.open();
+
+    folder = gui.addFolder('Time');
+    folder.add(p, 'light_travel_time').onChange(updateShader);
+    folder.add(p, 'time_scale').min(0);
+    //folder.open();
+
+}
+
+function onWindowResize( event ) {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    updateUniforms();
+}
+
+function initializeCamera(camera) {
+
+    var pitchAngle = 3.0, yawAngle = 0.0;
+
+    // there are nicely named methods such as "lookAt" in the camera object
+    // but there do not do a thing to the projection matrix due to an internal
+    // representation of the camera coordinates using a quaternion (nice)
+    camera.matrixWorldInverse.makeRotationX(degToRad(-pitchAngle));
+    camera.matrixWorldInverse.multiply(new THREE.Matrix4().makeRotationY(degToRad(-yawAngle)));
+
+    var m = camera.matrixWorldInverse.elements;
+
+    camera.position.set(m[2], m[6], m[10]);
+}
+
+function updateCamera( event ) {
+
+    var zoom_dist = camera.position.length();
+    var m = camera.matrixWorldInverse.elements;
+    var camera_matrix;
+
+    if (shader.parameters.observer.motion) {
+        camera_matrix = new THREE.Matrix3();
+    }
+    else {
+        camera_matrix = observer.orientation;
+    }
+
+    camera_matrix.set(
+        // row-major, not the same as .elements (nice)
+        // y and z swapped for a nicer coordinate system
+        m[0], m[1], m[2],
+        m[8], m[9], m[10],
+        m[4], m[5], m[6]
+    );
+
+    if (shader.parameters.observer.motion) {
+
+        observer.orientation = observer.orbitalFrame().multiply(camera_matrix);
+
+    } else {
+
+        var p = new THREE.Vector3(
+            camera_matrix.elements[6],
+            camera_matrix.elements[7],
+            camera_matrix.elements[8]);
+
+        var dist = shader.parameters.observer.distance;
+        observer.position.set(-p.x*dist, -p.y*dist, -p.z*dist);
+        observer.velocity.set(0,0,0);
+    }
+}
+
+function frobeniusDistance(matrix1, matrix2) {
+    var sum = 0.0;
+    for (var i in matrix1.elements) {
+        var diff = matrix1.elements[i] - matrix2.elements[i];
+        sum += diff*diff;
+    }
+    return Math.sqrt(sum);
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    camera.updateMatrixWorld();
+    camera.matrixWorldInverse.getInverse( camera.matrixWorld );
+
+    if (shader.needsUpdate || shader.hasMovingParts() ||
+        frobeniusDistance(camera.matrixWorldInverse, lastCameraMat) > 1e-10) {
+
+        shader.needsUpdate = false;
+        render();
+        lastCameraMat = camera.matrixWorldInverse.clone();
+    }
+    stats.update();
+}
+
+var lastCameraMat = new THREE.Matrix4().identity();
+
+var getFrameDuration = (function() {
+    var lastTimestamp = new Date().getTime();
+    return function() {
+        var timestamp = new Date().getTime();
+        var diff = (timestamp - lastTimestamp) / 1000.0;
+        lastTimestamp = timestamp;
+        return diff;
+    };
+})();
+
+function render() {
+    observer.move(getFrameDuration());
+    if (shader.parameters.observer.motion) updateCamera();
+    updateUniforms();
+    renderer.render( scene, camera );
+}
